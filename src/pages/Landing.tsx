76cdd5eaa4ab9e6ba4bdde4b65878a68a762551d
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-10-14 10:37:44 -0300

    orphanages map page finalized

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiArrowRight, FiPrinter } from 'react-icons/fi';
+import { FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
